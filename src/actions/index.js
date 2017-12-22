8138caafb38bf9474dff24ffe4388db77015feba
Tim Rourke <tim@timrourke.com>
2017-12-22 16:52:22 -0600

    Clean up syntax

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,23 +2,17 @@ export const ADD_TODO = 'ADD_TODO';
 export const COMPLETE_TODO = 'COMPLETE_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
 
-export function addTodo(task) {
-  return {
-    type: ADD_TODO,
-    task,
-  };
-}
+export const addTodo = (task) => ({
+  type: ADD_TODO,
+  task,
+});
 
-export function completeTodo(id) {
-  return {
-    type: COMPLETE_TODO,
-    id,
-  };
-}
+export const completeTodo = (id) => ({
+  type: COMPLETE_TODO,
+  id,
+});
 
-export function deleteTodo(id) {
-  return {
-    type: DELETE_TODO,
-    id,
-  };
-}
+export const deleteTodo = (id) => ({
+  type: DELETE_TODO,
+  id,
+});
