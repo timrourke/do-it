3a662b266f567a00288b9b13186fae0b1634d6b3
Tim Rourke <tim@timrourke.com>
2017-12-22 16:55:39 -0600

    Clean up syntax

--- src/containers/TodoListContainer.js
+++ src/containers/TodoListContainer.js
@@ -6,25 +6,21 @@ import {
   deleteTodo,
 } from './../actions/index';
 
-const mapStateToProps = state => {
-  return {
-    todos: state.todos,
-  };
-};
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addNewTodoTask(task) {
-      dispatch(addTodo(task));
-    },
-    completeTodoTask(id) {
-      dispatch(completeTodo(id));
-    },
-    deleteTodoTask(id) {
-      dispatch(deleteTodo(id));
-    },
-  };
-};
+const mapDispatchToProps = (dispatch) => ({
+  addNewTodoTask(task) {
+    dispatch(addTodo(task));
+  },
+  completeTodoTask(id) {
+    dispatch(completeTodo(id));
+  },
+  deleteTodoTask(id) {
+    dispatch(deleteTodo(id));
+  },
+});
 
 const TodoListContainer = connect(
   mapStateToProps,
